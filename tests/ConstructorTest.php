db10dba4dd8cb1904359bb33dc55fa989ba9b84c
KEINOS <github@keinos.com>
2020-07-20 21:20:14 +0900

    Update ConstructorTest.php

--- tests/ConstructorTest.php
+++ tests/ConstructorTest.php
@@ -12,6 +12,21 @@ final class ConstructorTest extends TestCase
         $this->assertIsObject($cache);
     }
 
+    public function testSetArgumentAsString()
+    {
+        $this->expectException(\TypeError::class);
+        $cache = new \KEINOS\MSTDN_TOOLS\Cache\Cache('namespace');
+    }
+
+    public function testSetNamespaceAsString()
+    {
+        $settings = [
+            'namespace' => hash('md5', strval(time())),
+        ];
+        $cache = new \KEINOS\MSTDN_TOOLS\Cache\Cache($settings);
+        $this->assertIsObject($cache);
+    }
+
     public function testSetPrefixAsString()
     {
         $settings = [
